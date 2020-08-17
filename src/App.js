565156229df36f8423db32502c70299ee6cb7647
aromsa <annacbateman@gmail.com>
2020-08-16 21:29:39 -0400

    grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from './Grid'
+
+const arrOfObj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 function App() {
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grid arrOfObj={arrOfObj}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
